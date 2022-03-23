e5efe6c6c76a3eb348ceb60aaeab00ff6beb60c1
gregoryblinks <ohwerhigregory@gmail.com>
2022-03-23 11:06:50 +0100

    First commit

--- app/main/test_routes.py
+++ app/main/test_routes.py
@@ -1,6 +1,7 @@
+from urllib import response
 import routes
-
 import unittest
+
 
 class MyTestCase(unittest.TestCase):
 
@@ -11,7 +12,10 @@ class MyTestCase(unittest.TestCase):
     def test_home(self):
         result = self.app.get('/')
         # Make your assertions
-        self.assertTrue(result, '/')
+        self.assertEqual(response.status_code,200) 
+
+#   from_list_to_string(deutsch, englisch)
+#   self.assertEqual(englisch)
 
 if __name__ == '__main__':
     unittest.main()
